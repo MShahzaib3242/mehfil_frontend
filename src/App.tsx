f634c12e00035b01f22423113860027ade6dfc29
Muhammad Shahzaib <shahzaib@softbuilders.ae>
2026-03-16 13:26:38 +0400

    Project Setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen text-3xl font-bold">
+      Mehfil - Where Stories Come Alive
+    </div>
+  );
+}
+
+export default App;
